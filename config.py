f6afd33b5f10eccced4af935a767d8a6180e5153
unlivedd <146029775+unlivedd@users.noreply.github.com>
2025-03-24 05:03:08 +0300

    Update config.py, fixed a moment that avg user cant get my own bot toker from .env file. So, I added it into config.p

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = '7728878781:AAHsQyAPeN1Ry9ycxoIp7I-sH4FFlUC34nE'
 MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "task_manager"
